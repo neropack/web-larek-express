fce3ce07511f11145f090fa2ed92c19984c142f4
Dmitriy Meshcheryakov <WindlessMist@yandex.ru>
2025-08-14 21:35:11 +0300

    feat product schema

---
+++ backend/models/product.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+interface IProduct {
+    title: string;
+    image: {
+        fileName: string;
+        originalName: string;
+    },
+    category: string;
+    description?: string;
+    price?: number | null;
+}
+
+const productSchema = new mongoose.Schema<IProduct>({
+  title: {
+    type: String, required: true, unique: true, minlength: 2, maxlength: 30,
+  },
+  image: { type: Object, required: true },
+  category: { type: String, required: true },
+  description: { type: String },
+  price: { type: Number, default: null },
+});
+
+export default mongoose.model<IProduct>('product', productSchema);
