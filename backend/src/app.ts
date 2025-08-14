f9de7581dcbb5a4e753e199ef3d0e1e1e099d3e5
Dmitriy Meshcheryakov <WindlessMist@yandex.ru>
2025-08-15 00:59:26 +0300

    ref/files-lower-in-branch

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,9 +4,9 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import path from 'path';
 
-import productRoutes from '../routes/product';
-import orderRoutes from '../routes/order';
-import errorMiddleware from '../middlewares/error-handler';
+import productRoutes from './routes/product';
+import orderRoutes from './routes/order';
+import errorMiddleware from './middlewares/error-handler';
 
 const PORT = process.env.PORT || 2000;
 const DB_ADD: string = process.env.DB_ADDRESS || 'mongodb://localhost:27017/mydb';
@@ -18,7 +18,7 @@ mongoose.connect(DB_ADD).then(() => {
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, './public')));
 
 app.use('/product', productRoutes);
 app.use('/order', orderRoutes);
