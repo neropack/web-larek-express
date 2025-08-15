ee59fa70851811bc50fb2253fc2fded75f647f16
Dmitriy Meshcheryakov <WindlessMist@yandex.ru>
2025-08-15 17:10:39 +0300

    ref/eslint

--- backend/src/controllers/order.ts
+++ backend/src/controllers/order.ts
@@ -2,14 +2,15 @@ import { Request, Response, NextFunction } from 'express';
 import { faker } from '@faker-js/faker';
 import Product from '../models/product';
 import BadRequestError from '../errors/bad-request-error';
-import { ad } from '@faker-js/faker/dist/airline-CLphikKp';
 
 const createOrder = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { total, items, payment, email, phone, address } = req.body;
+    const {
+      total, items, payment, email, phone, address,
+    } = req.body;
 
     if (!total || !items || !payment || !email || !phone || !address) {
-      throw new Error('Данные отсутствуют')
+      throw new Error('Данные отсутствуют');
     }
 
     if (!Array.isArray(items) || items.length === 0) {
